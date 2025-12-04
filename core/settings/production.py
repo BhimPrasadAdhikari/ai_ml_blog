c097345fba11a06dd4509f7fed003b83b9534c2e
bhimprasadadhikari <134574549+BhimPrasadAdhikari@users.noreply.github.com.>
2025-12-04 21:29:56 +0545

    modified:   core/settings/production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -39,15 +39,16 @@ DATABASES = {
     )
 }
 
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Email configuration for production 
 if not os.environ.get("DEMO_MODE"):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = "smtp.gmail.com"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+   
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
